6b3a768c83c57b21d48d85bcc7ac9fbe10d481b0
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2024-03-01 01:14:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+ 
+/*
+Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+
+ var number1=+prompt('Enter any number for addition' )
+ var number2=+prompt('Enter any number for addition' )
+ var sum=number1+number2
+ document.write('The sum of \n'+number1+  '\nand \n' +number2+ '\nis\n' +sum+ "<br>")
+
+ var number1=+prompt('Enter any number for subtraction')
+ var number2=+prompt('Enter any number for subtraction')
+ var sub=number1-number2
+ document.write('\nThe differnce of \n'+number1+  '\nand \n' +number2+ '\n is \n' +sub+'<br>' )
+
+ var number1=+prompt('Enter any number for multiplication')
+ var number2=+prompt('Enter any number for multiplication')
+ var mul=number1*number2
+ document.write('\nThe product of \n'+number1+  '\nand \n' +number2+ '\n is \n' +mul+ "<br>" )
+
+ var number1=+prompt('Enter any number for division')
+ var number2=+prompt('Enter any number for division')
+ var div=number1/number2
+ document.write('\nThe division of \n'+number1+  '\nand \n' +number2+ '\n is \n' +div+'<br>' )
+ */
+  
+
+
+//Assignment 2:
+
+
+//  var sen='Value after variable declaration is undefined'+""
+//  document.write(sen+"<br>")
+//  var initialvalue=5 //5
+//  document.write("initial value:"+initialvalue+'<br>')//5
+//  initialvalue++//5
+//  document.write('Value after increment is \n'+initialvalue+'<br>')//6
+//  var num =7
+//  var sum=initialvalue+num
+//  document.write('number after addition is\n'+sum+'<br>')
+//  --sum
+//  document.write('Value after decrement is'+sum+'<br>')
+//  var div=sum/12
+//  document.write('The remainder is\n'+div)
+
+
+
+
+
+//Assignment 3: Make a table of 4
+// var num=4
+// document.write('Table of 4 is '+'<br>')
+// var num1=num*1
+// document.write('4x1='+num1+'<br>' )
+// var num2=num*2
+// document.write('4x2='+num2+'<br>' )
+// var num3=num*3
+// document.write('4x3='+num3+'<br>' )
+// var num4=num*4
+// document.write('4x4='+num4+'<br>' )
+// var num5=num*5
+// document.write('4x5='+num5+'<br>' )
+// var num6=num*6
+// document.write('4x6='+num6+'<br>' )
+// var num7=num*7
+// document.write('4x7='+num7+'<br>' )
+// var num8=num*8
+// document.write('4x8='+num8+'<br>' )
+// var num9=num*9
+// document.write('4x9='+num9+'<br>' )
+// var num10=num*10
+// document.write('4x10 ='+num10+'<br>' )
+
+
+//Assignment 4:
+// var num=600
+// document.write('The cost of one ticket is :'+num+'<br>')
+// var num1=5*600
+// document.write('The cost of five ticket is :'+num1)
+
+
+
+
+
+
+var celsiustemperature=12
+document.write('The temperature in celsius is:'+celsiustemperature+'C'+'<br>')
+var conversion=(22-32)*5/9
+document.write('12C is'+conversion+'F'+"<br>")
+var farenheightemperature=22
+document.write('The temperature in farenheight is:'+farenheightemperature+'f'+'<br>')
+var conversion1=(12*9/5)+32
+document.write('22F is'+conversion1+'C'+"<br>")
